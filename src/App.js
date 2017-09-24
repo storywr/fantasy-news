20dc79f405a20001155c6537f75674828037cef4
Bill Story <bstory@k2p.com>
2017-09-24 00:13:44 -0400

    Fix Drawer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,102 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import MiniDrawer from './containers/MiniDrawer/index'
-import './App.css'
+import { withStyles } from 'material-ui/styles'
+import Drawer from 'material-ui/Drawer'
+import AppBar from 'material-ui/AppBar';
+import List, {
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+} from 'material-ui/List';
+import Divider from 'material-ui/Divider'
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
 
-const App = ({ children }) => (
-  <MiniDrawer>
-    {children}
-  </MiniDrawer>
-)
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: '100vh',
+    marginTop: theme.spacing.unit * 3,
+    zIndex: 1,
+    overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    position: 'absolute',
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    order: 1,
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: drawerWidth,
+  },
+  drawerHeader: theme.mixins.toolbar,
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64,
+    },
+  },
+});
+
+function App(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <div className={classes.appFrame}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography type="title" color="inherit" noWrap>
+              Fantasy Football News
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          type="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader} />
+          <Divider />
+          <List>
+            <ListItem button>
+              <ListItemText
+                primary='Players'
+              />
+            </ListItem>
+            <ListItem button>
+              <ListItemText
+                primary='News'
+              />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <Typography type="body1" noWrap>
+            {props.children}
+          </Typography>
+        </main>
+      </div>
+    </div>  )
+}
 
 App.propTypes = {
   children: PropTypes.node
@@ -17,4 +106,4 @@ App.defaultProps = {
   children: null
 }
 
-export default App
+export default withStyles(styles)(App)
